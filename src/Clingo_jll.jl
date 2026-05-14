dcc26bc412ac9d706f4abf7e30e52c06065f4b5f
jlbuild <juliabuildbot@gmail.com>
2026-05-14 22:52:47 +0000

    Clingo_jll build 5.8.0+0

--- src/Clingo_jll.jl
+++ src/Clingo_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Clingo")
-JLLWrappers.@generate_main_file("Clingo", UUID("e1d4ad0e-dc8b-5d2d-8155-e9a55b531f08"))
+JLLWrappers.@generate_main_file("Clingo", Base.UUID("e1d4ad0e-dc8b-5d2d-8155-e9a55b531f08"))
 end  # module Clingo_jll
